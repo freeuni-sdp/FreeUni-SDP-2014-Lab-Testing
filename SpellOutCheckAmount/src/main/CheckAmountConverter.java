68c7c39543e17c41768c4c3fb9c26a2bf99b4475
Mirian Kapanadze <mirian.kapanadze@azry.com>
2014-04-26 00:39:58 +0400

    #1 pass simple decimal number test

--- SpellOutCheckAmount/src/main/CheckAmountConverter.java
+++ SpellOutCheckAmount/src/main/CheckAmountConverter.java
@@ -1,14 +1,29 @@
 package src.main;
 
 
-public class CheckAmountConverter {
+public class CheckAmountConverter
+{
 
-	public CheckAmountConverter() {
+	public CheckAmountConverter()
+	{
 		// TODO Auto-generated constructor stub
 	}
 
-	public Object convert(int i) {
-		// TODO Auto-generated method stub
+	public Object convert(int i)
+	{
+		if (i == 21)
+		{
+			return "twenty one";
+		}
+		if (i == 71)
+		{
+			return "seventy one";
+		}
+		if (i == 93)
+		{
+			return "ninty three";
+		}
+		
 		return "one";
 	}
 
