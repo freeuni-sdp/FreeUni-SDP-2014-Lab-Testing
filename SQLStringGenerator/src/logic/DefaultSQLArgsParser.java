f98b994b8b8a0e4318977c7a9e26c97d83613864
vatm1010 <vatm1010@gmail.com>
2014-04-25 21:40:52 +0400

    #4 added test for standard column parsing

---
+++ SQLStringGenerator/src/logic/DefaultSQLArgsParser.java
@@ -0,0 +1,13 @@
+package logic;
+
+public class DefaultSQLArgsParser implements SQLArgsParser {
+
+	public String parseColumns(String[] columns) {
+		return null;
+	}
+
+	public String parseValues(String[] values) {
+		return null;
+	}
+
+}
---
+++ SQLStringGenerator/test/test/TestSQLArgsParser.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.Assert.*;
+import logic.DefaultSQLArgsParser;
+import logic.SQLArgsParser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSQLArgsParser {
+	private SQLArgsParser parser;
+
+	@Before
+	public void initialize() {
+		parser = new DefaultSQLArgsParser();
+	}
+
+	@Test
+	public void shouldReturnStringOfColumnNamesSeparatedByCommas() {
+		String[] columns = { "col1", "col2", "col3" };
+
+		String expected = columns[0] + ", " + columns[1] + ", " + columns[2];
+
+		assertEquals(expected, parser.parseColumns(columns));
+	}
+
+}
