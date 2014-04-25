985fc293bf9342323d0693f2335587e9f90f49bc
vatm1010 <vatm1010@gmail.com>
2014-04-25 21:43:39 +0400

    #4 standard column parsing test passed

--- SQLStringGenerator/src/logic/DefaultSQLArgsParser.java
+++ SQLStringGenerator/src/logic/DefaultSQLArgsParser.java
@@ -1,9 +1,11 @@
 package logic;
 
+import java.util.Arrays;
+
 public class DefaultSQLArgsParser implements SQLArgsParser {
 
 	public String parseColumns(String[] columns) {
-		return null;
+		return Arrays.toString(columns).replace("[", "").replace("]", "");
 	}
 
 	public String parseValues(String[] values) {
