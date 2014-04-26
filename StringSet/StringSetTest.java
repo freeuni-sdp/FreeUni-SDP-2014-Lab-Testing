adbd774fdb7fe9c6a8caba700f98eb4982cc50d3
kesha <tkesh11@freeuni.edu.ge>
2014-04-27 01:58:33 +0400

    enumerator method test. final changes

--- StringSet/StringSetTest.java
+++ StringSet/StringSetTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
@@ -7,41 +8,42 @@ import java.util.Set;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class StringSetTest {
 
 	private StringSet set;
-	
+
 	@Before
 	public void createSet() {
 		set = new StringSet();
 	}
-	
+
 	@Test
 	public void addTest() {
-		assertEquals("count should return 0, because there is no elemenet", 0, set.count());
+		assertEquals("count should return 0, because there is no elemenet", 0,
+				set.count());
 		set.add("newString");
 		assertEquals("count should return 1", 1, set.count());
 		set.add("newString");
 		assertEquals("count should return 1", 1, set.count());
 	}
-	
+
 	@Test
 	public void removeTest() {
 		set.add("newString");
 		assertTrue(set.remove("newString"));
-		
+
 	}
-	
+
 	@Test
 	public void clearTest() {
 		set.add("newString");
 		set.add("second string");
 		set.clear();
-		assertEquals("count should return 0, because set is cleared", 0, set.count());
-		
+		assertEquals("count should return 0, because set is cleared", 0,
+				set.count());
+
 	}
-	
+
 	@Test
 	public void countTest() {
 		set.add("newString");
@@ -49,20 +51,22 @@ public class StringSetTest {
 		set.add("second string");
 		assertEquals("count should return 2", 2, set.count());
 		assertTrue(set.remove("newString"));
-		assertEquals("count should return 1, because newString is deleted", 1, set.count());
+		assertEquals("count should return 1, because newString is deleted", 1,
+				set.count());
 		set.clear();
-		assertEquals("count should return 0, because set is cleared", 0, set.count());
-		
+		assertEquals("count should return 0, because set is cleared", 0,
+				set.count());
+
 	}
-	
+
 	@Test
 	public void containsTest() {
 		set.add("newString");
 		assertTrue(set.contains("newString"));
 		assertFalse(set.contains("aa"));
-		
+
 	}
-	
+
 	@Test
 	public void unionTest1() {
 		Set<String> s = new HashSet<String>();
@@ -70,10 +74,9 @@ public class StringSetTest {
 		set.add("newString");
 		set.union(s);
 		assertEquals("count should return 2", 2, set.count());
-		
-		
+
 	}
-	
+
 	public void unionTest2() {
 		Set<String> s = new HashSet<String>();
 		s.add("a");
@@ -81,17 +84,18 @@ public class StringSetTest {
 		set.add("newString");
 		set.union(s);
 		assertEquals("count should return 2", 2, set.count());
-		
+
 	}
+
 	public void unionTest3() {
 		Set<String> s = new HashSet<String>();
 		set.union(s);
 		assertEquals("count should return 0", 0, set.count());
-		
+
 		set.add("aa");
 		assertEquals("count should return 1", 1, set.count());
 	}
-	
+
 	@Test
 	public void intersectionTest1() {
 		Set<String> s = new HashSet<String>();
@@ -99,9 +103,9 @@ public class StringSetTest {
 		set.add("a");
 		set.intersection(s);
 		assertEquals("count should return 1", 1, set.count());
-		
+
 	}
-	
+
 	@Test
 	public void intersectionTest2() {
 		Set<String> s = new HashSet<String>();
@@ -110,26 +114,39 @@ public class StringSetTest {
 		set.add("a");
 		set.intersection(s);
 		assertEquals("count should return 1", 1, set.count());
-		
+
 	}
-	
+
 	@Test
 	public void intersectionTest3() {
 		Set<String> s = new HashSet<String>();
 		set.intersection(s);
 		assertEquals("count should return 0", 0, set.count());
-		
+
 		set.add("aa");
 		assertEquals("count should return 1", 1, set.count());
-		
+
 	}
-	
+
 	@Test
-	public void enumeratorTest() {
+	public void enumeratorTest1() {
 		set.add("elem");
-		Enumeration<String> a = set.enumerator();
-		assertEquals("answer will be elem", "elem", a.nextElement());
-		
+		Enumeration<String> enumer = set.enumerator();
+		assertEquals("answer will be elem", "elem", enumer.nextElement());
+	}
+
+	@Test
+	public void enumeratorTest2() {
+		set.add("a");
+		set.add("b");
+		set.add("c");
+		set.add("d");
+		Enumeration<String> enumer = set.enumerator();
+		ArrayList<String> helper = new ArrayList<String>();
+		while (enumer.hasMoreElements())
+			helper.add(enumer.nextElement());
+
+		assertEquals("helpers size must be 4", 4, helper.size());
 	}
 
 }
