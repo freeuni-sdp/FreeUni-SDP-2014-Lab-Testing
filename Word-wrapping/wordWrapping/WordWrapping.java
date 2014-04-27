d3bb92fcd4c6b712f19bbfa32ed65f8f9f0af2c6
salometsiramua <stsir11@freeuni.edu.ge>
2014-04-27 22:51:29 +0400

    #6 refactoring and adding implementation for bigger input lengths

--- Word-wrapping/wordWrapping/WordWrapping.java
+++ Word-wrapping/wordWrapping/WordWrapping.java
@@ -9,17 +9,22 @@ public class WordWrapping {
 	
 	public static ArrayList<String> wordWrap(String text, int length){
 		ArrayList<String> resultStrings = new ArrayList<>();
-		if(text.length() < length)
+		
+		if(text.length() < length){
 			resultStrings.add(text);
-		else{
-			for(int i = 0; i < text.length(); i++){
-				String currString = "";
-				for(int j = 0; j < length; j++){
-					currString += text.charAt(i + j);
-				}
-				resultStrings.add(currString);
+		}else{
+			int startPos = 0;
+			while(startPos < text.length()){
+			
+				String currString = text.substring(startPos, length + startPos);
+				resultStrings.add(resultStrings.size(), currString);
+				startPos = startPos + length;
+				
 			}
+			
 		}
+		
+		
 		return resultStrings;
 		
 		
