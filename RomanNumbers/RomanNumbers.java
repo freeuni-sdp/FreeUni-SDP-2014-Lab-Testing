f42bddc6be3407e1c922f924502ae1ea4ba05e4b
eleneakhvlediani <eakhv11@freeuni.edu.ge>
2014-04-27 14:54:12 +0400

    #8 passed for one

--- RomanNumbers/RomanNumbers.java
+++ RomanNumbers/RomanNumbers.java
@@ -1,8 +1,9 @@
 
 public class RomanNumbers {
 
+	
 	public static String intToRoman(int number){
-		return null;
+		return "I";
 		
 	}
 	
@@ -10,5 +11,9 @@ public class RomanNumbers {
 		
 		return 0;
 		
+	
+	}
+	public static void main(String[] args) {
+		System.out.println("SSSS");
 	}
 }
--- RomanNumbers/RomanNumbersTest.java
+++ RomanNumbers/RomanNumbersTest.java
@@ -10,5 +10,11 @@ public class RomanNumbersTest {
 		
 		assertEquals("I", RomanNumbers.intToRoman(1));
 	}
+	
+	@Test
+	public void testIntToRomanForOtherNumbersWithOneSymbol() {
+		
+		assertEquals("V", RomanNumbers.intToRoman(5));
+	}
 
 }
