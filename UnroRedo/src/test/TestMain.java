164d27b87a528ed2d4bc3b15c64f2334c89ffb2b
Nodarius <n0dari@yahoo.com>
2014-04-27 14:05:25 +0400

    #12 undo_redo when empty, tests added

--- UnroRedo/src/test/TestMain.java
+++ UnroRedo/src/test/TestMain.java
@@ -16,7 +16,15 @@ public class TestMain {
 		main = new Main();
 	}
 
-
-
+	@Test
+	public void shouldReturnErrorWhenUndoingWhenEmpty(){
+		assertEquals(main.undo(), main.ERROR);
+	}
+	
+	@Test
+	public void shouldReturnErrorWhenRedoingWhenEmpty(){
+		assertEquals(main.redo(), main.ERROR);
+	}
+	
 
 }
