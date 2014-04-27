ffde27ae8631eac8c0f2f7103ab3d84c7b3f0d09
takojaparidze <tjaph10@freeuni.edu.ge>
2014-04-27 13:11:26 +0400

    #7 tests the tab and enter cases at the end of the word

--- endOfLineTrimming/Trimming.java
+++ endOfLineTrimming/Trimming.java
@@ -7,13 +7,22 @@ public class Trimming {
 	public String lastOutput(String text) {
 		if (text.equals(""))
 			return text;
-		
+		text = check(text, "\t");
+		text = check(text, "\n");
 		for (int i = text.length() - 1; i >= 0; i--) {
-			if(text.charAt(i) != ' '){
-				text =  text.substring(0,i+1);
+			if (text.charAt(i) != ' ') {
+				text = text.substring(0, i + 1);
 				break;
 			}
 		}
+		return text;
+	}
+
+	/* zedmet spacebs moashlis tabis , enteris da a.sh shemtxvevashi */
+	private String check(String text, String sub) {
+		
+		if (text.substring(text.length() - 1).equals(sub))
+			text = text.substring(0, text.length() - 1);
 		return text;
 
 	}
--- endOfLineTrimming/TrimmingTest.java
+++ endOfLineTrimming/TrimmingTest.java
@@ -7,12 +7,17 @@ public class TrimmingTest {
 	Trimming trm = new Trimming();
 	
 	@Test
-	public void test() {
+	public void testSpaceCases() {
 		assertEquals("lastOutput method should return empty String ", "", trm.lastOutput(""));
 		assertEquals("lastOutput method should remove the space at the end ", "tako", trm.lastOutput("tako "));
 		assertEquals("lastOutput method should remove all spaces at the end ", "tako", trm.lastOutput("tako    "));
 		assertEquals("lastOutput method should not remove the space in the word ", " tako japaridze", trm.lastOutput(" tako japaridze"));
-
+	}
+	
+	@Test
+	public void testTabsAndEnterCases() {
+		assertEquals("lastOutput method should remove the tabs at the and of the string ", "tako", trm.lastOutput("tako\t"));
+		assertEquals("lastOutput method should remove the Enter at the end of the string ", "tako", trm.lastOutput("tako\n"));
 	}
 
 }
