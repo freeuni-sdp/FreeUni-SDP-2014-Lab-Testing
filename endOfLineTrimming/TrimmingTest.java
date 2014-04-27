6638aa460682b8222ce3b2ad23db23bc66a3460d
takojaparidze <tjaph10@freeuni.edu.ge>
2014-04-27 12:51:47 +0400

    #7 test about space at the beginning and the middle of the word

--- endOfLineTrimming/TrimmingTest.java
+++ endOfLineTrimming/TrimmingTest.java
@@ -11,6 +11,7 @@ public class TrimmingTest {
 		assertEquals("lastOutput method should return empty String ", "", trm.lastOutput(""));
 		assertEquals("lastOutput method should remove the space at the end ", "tako", trm.lastOutput("tako "));
 		assertEquals("lastOutput method should remove all spaces at the end ", "tako", trm.lastOutput("tako    "));
+		assertEquals("lastOutput method should not remove the space in the word ", " tako japaridze", trm.lastOutput(" tako japaridze"));
 
 	}
 
