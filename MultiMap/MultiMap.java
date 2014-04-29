86e7001d9b76336406cafe0229fcda027a656784
giorgi-matiashvili <gmati11@freeuni.edu.ge>
2014-04-29 20:58:33 +0400

    #10 test what I put in map is what I get from it

--- MultiMap/MultiMap.java
+++ MultiMap/MultiMap.java
@@ -1,12 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class MultiMap<K, V extends List<?>> {
 	
+	private ArrayList<K> keys;
+	private ArrayList<V> values;
+	
 	public MultiMap(){
+		keys = new ArrayList<K>();
+		values = new ArrayList<V>();
 	}
 	
 	public int size(){
 		return 0;
+	}
+	
+	public void put(K key, V value){
+		keys.add(key);
+		values.add(value);
+	}
+	
+	public List<K> get(){
+		return (List<K>) keys.clone();
 	}
 }
--- MultiMap/MultiMapTest.java
+++ MultiMap/MultiMapTest.java
@@ -12,5 +12,11 @@ public class MultiMapTest {
 		MultiMap<String, List<Integer>> mp = new MultiMap<String, List<Integer>>();
 		assertEquals(0, mp.size());
 	}
-
+	
+	@Test
+	public void testWhatIPutIsWhatIGet(){
+		MultiMap<Integer, List<String>> mp = new MultiMap<Integer, List<String>>();
+		mp.put(1, new ArrayList<String>());
+		assertEquals(new Integer(1), mp.get().get(0));
+	}
 }
